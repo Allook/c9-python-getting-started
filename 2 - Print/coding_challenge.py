9a3a1f01ea48ee155a9939b8ca92d09961f2d0d7
Allook <55939806+Allook@users.noreply.github.com>
2019-09-29 16:32:47 +0800

    Update coding_challenge.py

--- 2 - Print/coding_challenge.py
+++ 2 - Print/coding_challenge.py
@@ -5,11 +5,11 @@
 print('Why won't this line of code print')
 
 # print the message
-prnit('This line fails too!')
+print('This line fails too!')
 
 # print the message
 print "I think I know how to fix this one"
 
 # print the name entered by the user
-input('Please tell me your name: ')
+name=input('Please tell me your name: ')
 print(name)
